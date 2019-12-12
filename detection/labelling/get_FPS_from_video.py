24886af50ea4c5e65a8c4207028c10b537cfe3ea
Timm Kühnel <t_kueh06@uni-muenster.de>
2019-12-12 09:49:14 +0100

    Add first VideoLabelling Element as singular script

---
+++ detection/labelling/get_FPS_from_video.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import numpy
+import cv2
+import sys
+if __name__ == '__main__' :
+	if len(sys.argv) > 1:
+		try:
+			with open(sys.argv[1], 'r') as fh:
+				video = cv2.VideoCapture(sys.argv[1]);
+
+				# Find OpenCV version
+				(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+
+				if int(major_ver)  < 3 :
+					fps = video.get(cv2.cv.CV_CAP_PROP_FPS)
+					print(f"Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {fps}")
+				else :
+					fps = video.get(cv2.CAP_PROP_FPS)
+					print(f"Frames per second using video.get(cv2.CAP_PROP_FPS) : {fps}")
+
+				video.release(); 
+		except FileNotFoundError:
+			print("Please pass a valid path to a video file as the first CL parameter.")
+	else:
+		print("Please pass a valid path to a video file as the first CL parameter.")
